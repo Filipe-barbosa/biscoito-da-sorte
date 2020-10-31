d6232e7c03ff2d7efbda9feb1ddbbfbae1c48579
unknown <filipebarbosa94@hotmail.com>
2020-10-31 13:24:48 -0300

    Updates

---
+++ precache-manifest.56f7761bb99420d44b95c652e487caa5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aecbabae5a58d8ef6e7fb82053b816a1",
+    "url": "/biscoito-da-sorte/index.html"
+  },
+  {
+    "revision": "4a2739d19ddb62d78444",
+    "url": "/biscoito-da-sorte/static/css/main.e880e560.chunk.css"
+  },
+  {
+    "revision": "6cd102f999298f4ee849",
+    "url": "/biscoito-da-sorte/static/js/2.b6e2c613.chunk.js"
+  },
+  {
+    "revision": "961cf508e89c6b167b43678b586da794",
+    "url": "/biscoito-da-sorte/static/js/2.b6e2c613.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a2739d19ddb62d78444",
+    "url": "/biscoito-da-sorte/static/js/main.861e6c11.chunk.js"
+  },
+  {
+    "revision": "99c7f5c80af863033551",
+    "url": "/biscoito-da-sorte/static/js/runtime-main.b6705803.js"
+  },
+  {
+    "revision": "a2bd231ca8e65322172513b0b27e00b7",
+    "url": "/biscoito-da-sorte/static/media/biscoito.a2bd231c.png"
+  }
+]);
